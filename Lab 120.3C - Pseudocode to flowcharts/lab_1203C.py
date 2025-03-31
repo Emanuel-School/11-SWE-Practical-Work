e586176ebb2b69140db6c049612fed9503f7b3f0
gellert.alexander <gellert.alexander@emanuelschool.nsw.edu.au>
2025-04-01 10:46:11 +1100

    Added Labs 120.3A-C

--- Lab 120.3C - Pseudocode to flowcharts/lab_1203C.py
+++ Lab 120.3C - Pseudocode to flowcharts/lab_1203C.py
@@ -1,18 +1,18 @@
 from math import sqrt
 
 num = int(input("Enter a number: "))
+prime = True
 
 if num < 2:
     print("Not prime")
-else:
-    prime = True
-
-for i in range(2, int(sqrt(num))):
+    prime = False
+for i in range(2, int(sqrt(num))+1):
     if num % i == 0:
         prime = False
         break
-    if prime == True:
-        print("prime")
-    else:
-        print("not prime")
+
+if prime == True:
+    print("prime")
+else:
+    print("not prime")
 
